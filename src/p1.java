77689d10bf03855943eebf418c5cb15f824c57a9
victor <baiesvictor2590@gmail.com>
2016-04-13 18:33:58 +0300

    Teme

---
+++ src/p1.java
@@ -0,0 +1,15 @@
+public class p1 extends SkeletonJava {
+
+    public static void main(String[] args){
+
+        //1.     Sa se calculeze media aritmetica a doua numere.
+
+        float a= readIntGUI("Introduceti prima nota: ");
+        float b= readIntGUI("Introduceti a doua nota: ");
+        float med=(a+b)/2;
+        printGUI("Media aritmetica este: "+med);
+
+
+
+    }
+}

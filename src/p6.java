116f16a217541ef9723722a8034cfad56f512e36
victor <baiesvictor2590@gmail.com>
2016-04-14 18:39:51 +0300

    Teme

---
+++ src/p6.java
@@ -0,0 +1,47 @@
+public class p6 extends SkeletonJava {
+
+    public static void main(String[] args) {
+
+/*
+Se da un sir de numere. Sa se afiseze:
+numarul maxim, numarul minim, suma lor, sirul dublat .
+*/
+
+        int n[] = {2,4,7,3,5,6};
+
+        int min = n[0];
+        int max = n[0];
+        int sum = n[0];
+        int dub = n[0];
+
+        for(int i=0; i<n.length; i++) {
+            if (n[i] > max)
+                max = n[i];
+
+            if (n[i] < min)
+                min = n[i];
+
+            sum = sum + n[i];
+
+        }
+
+            System.out.println(" ");
+            System.out.println("Cel mai mare numar: " + max);
+            System.out.println("Cel mai mic numar: " + min);
+            System.out.println("Suma numerelor din sir: " + sum);
+
+
+        for (int i=0; i<n.length; i++) {
+            for (int j = 0; j < n.length; j++) {
+                n[i] = 2 * n[j];
+
+                System.out.print(" " + n[i]);
+            }break;
+
+
+        }
+
+
+
+    }
+}
